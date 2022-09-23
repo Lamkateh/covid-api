ec063f9e14d4da214b97b221de35819e621e3d8a
Ethan ARMUTLU <ethan.armutlu7@etu.univ-lorraine.fr>
2022-09-23 17:53:05 +0200

    Creation CenterController

---
+++ src/main/java/org/polytech/covidapi/controllers/CenterController.java
@@ -0,0 +1,42 @@
+package org.polytech.covidapi.controllers;
+
+import org.polytech.covidapi.entities.Center;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class CenterController {
+    
+    private final CenterRepository centerRepository;
+
+    CenterController(CenterRepository centerRepository){
+        this.centerRepository = centerRepository;
+    }
+
+    @GetMapping("/public/centers/{city}")
+    Page<Center> findAllByCity(@PathVariable String city, @PageableDefault(size = 15) Pageable p) {
+        return centerRepository.findAllByCity(city, p);
+    }
+
+    @GetMapping("/public/centers/{name}")
+    Page<Center> findAllByName(@PathVariable String name, @PageableDefault(size = 15) Pageable p) {
+        return centerRepository.findAllByCity(name, p);
+    }
+
+    @PostMapping("/admin/center")
+    Center store(@RequestBody Center center){
+        // Récupère données user connectée   A VOIR AVEC COURS JWT
+        // checker rôle
+        Center centerSaved = new Center(center.getName(), center.getCity(), center.getZipCode(), center.getAddress(), center.getPhone(), center.getEmail());
+        centerRepository.save(centerSaved);
+        return centerRepository.findFirstById(centerSaved.getId());
+    }
+
+
+
+
+}
---
+++ src/main/java/org/polytech/covidapi/controllers/UserController.java
@@ -0,0 +1,15 @@
+package org.polytech.covidapi.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+public class AuthController {
+
+    private final UserRepository userRepository;
+
+    
+
+
+}
