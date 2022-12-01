d8faf4d87d841530aadf09d25b1a3d9d41d7dd56
Gaëtan NOUSSE <gaetannousse@gmail.com>
2022-12-01 18:42:26 +0100

    Add update and delete user

--- src/main/java/org/polytech/covidapi/controllers/user/UserController.java
+++ src/main/java/org/polytech/covidapi/controllers/user/UserController.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 import org.polytech.covidapi.dao.CenterRepository;
 import org.polytech.covidapi.dao.UserRepository;
+import org.polytech.covidapi.dto.ProfileView;
 import org.polytech.covidapi.dto.SignupUserView;
 import org.polytech.covidapi.entities.Center;
 import org.polytech.covidapi.entities.ERole;
@@ -20,11 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -60,15 +57,12 @@ public class UserController {
 
     @PostMapping(path = "/private/users")
     public ResponseEntity<Object> store(@RequestBody SignupUserView userSignup) throws ResourceNotFoundException {
-        /*
-         * if (!authenticationFacade.hasRole(ERole.ADMIN) &&
-         * !authenticationFacade.hasRole(ERole.SUPER_ADMIN)) {
-         * return ResponseHandler.
-         * generateResponse("You are not allowed to access this resource",
-         * HttpStatus.FORBIDDEN,
-         * null);
-         * }
-         */
+         if (!authenticationFacade.hasRole(ERole.ADMIN) && !authenticationFacade.hasRole(ERole.SUPER_ADMIN)) {
+            return ResponseHandler.
+            generateResponse("You are not allowed to access this resource",
+            HttpStatus.FORBIDDEN,
+            null);
+         }
 
         Optional<User> userSearch = userRepository.findFirstByEmail(userSignup.getEmail());
         if (userSearch.isPresent()) {
@@ -80,14 +74,10 @@ public class UserController {
         user.setEmail(userSignup.getEmail());
         user.setBirthDate(userSignup.getBirthDate());
         user.setPhone(userSignup.getPhone());
-        System.out.println(userSignup.getCenter_id());
-        if (userSignup.getCenter_id() != null) {
-            System.out.println("test");
-            Center center = centerRepository.findById(userSignup.getCenter_id())
+        if (userSignup.getCenterId() != null) {
+            Center center = centerRepository.findById(userSignup.getCenterId())
                     .orElseThrow(() -> new ResourceNotFoundException("Center not found"));
-            System.out.println(userSignup.getRoles().get(0));
             if (userSignup.getRoles().get(0).equals("DOCTOR")) {
-                System.out.println("test");
                 user.setCenter(center);
                 center.addDoctor(user);
             }
@@ -109,4 +99,67 @@ public class UserController {
                 userRepository.save(user));
     }
 
+    @PutMapping(path = "/private/users/{id}")
+    public ResponseEntity<Object> update(@PathVariable int id, @RequestBody SignupUserView userDetails)
+            throws ResourceNotFoundException {
+        if (!authenticationFacade.hasRole(ERole.ADMIN)) {
+            return ResponseHandler.generateResponse("You are not allowed to access this resource", HttpStatus.FORBIDDEN,
+                    null);
+        }
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        // TODO : better validation
+        if (userDetails.getFirstName() == null || userDetails.getFirstName().isEmpty()) {
+            return ResponseHandler.generateResponse("First name is required", HttpStatus.BAD_REQUEST, null);
+        }
+        if (userDetails.getLastName() == null || userDetails.getLastName().isEmpty()) {
+            return ResponseHandler.generateResponse("Last name is required", HttpStatus.BAD_REQUEST, null);
+        }
+        if (userDetails.getEmail() == null || userDetails.getEmail().isEmpty()) {
+            return ResponseHandler.generateResponse("Email is required", HttpStatus.BAD_REQUEST, null);
+        }
+        if (userDetails.getPhone() == null || userDetails.getPhone().isEmpty()) {
+            return ResponseHandler.generateResponse("Phone is required", HttpStatus.BAD_REQUEST, null);
+        }
+        if (userDetails.getBirthDate() == null || userDetails.getBirthDate().toString().isEmpty()) {
+            return ResponseHandler.generateResponse("Birth date is required", HttpStatus.BAD_REQUEST, null);
+        }
+        if (userDetails.getPassword() == null || userDetails.getPassword().isEmpty()) {
+            return ResponseHandler.generateResponse("Password is required", HttpStatus.BAD_REQUEST, null);
+        }
+        if (userDetails.getRoles() == null || userDetails.getRoles().isEmpty()) {
+            return ResponseHandler.generateResponse("Role is required", HttpStatus.BAD_REQUEST, null);
+        }
+        if (userDetails.getCenterId() == null || userDetails.getCenterId().toString().isEmpty()) {
+            return ResponseHandler.generateResponse("Center id is required", HttpStatus.BAD_REQUEST, null);
+        }
+
+        Center center = centerRepository.findById(userDetails.getCenterId())
+                .orElseThrow(() -> new ResourceNotFoundException("Center not found"));
+
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setEmail(userDetails.getEmail());
+        user.setPhone(userDetails.getPhone());
+        user.setBirthDate(userDetails.getBirthDate());
+        user.setPassword(passwordEncoder.encode(userDetails.getPassword()));
+        user.setPhone(userDetails.getPhone());
+        user.setRoles(userDetails.getRoles());
+        user.setCenter(center);
+        User updatedUser = userRepository.save(user);
+        return ResponseHandler.generateResponse("User successfully updated", HttpStatus.OK, updatedUser);
+    }
+
+    @DeleteMapping(path = "/private/users/{id}")
+    public ResponseEntity<Object> delete(@PathVariable int id) {
+        if (!authenticationFacade.hasRole(ERole.ADMIN)) {
+            return ResponseHandler.generateResponse("You are not allowed to access this resource", HttpStatus.FORBIDDEN,
+                    null);
+        }
+
+        userRepository.deleteById(id);
+        return ResponseHandler.generateResponse("User successfully deleted", HttpStatus.OK, null);
+    }
 }
