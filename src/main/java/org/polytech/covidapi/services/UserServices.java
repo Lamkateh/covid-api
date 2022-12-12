8421f5e9868cc842d9c5a9a6926247cab4fa4cb5
Gaëtan NOUSSE <gaetannousse@gmail.com>
2022-12-12 11:01:57 +0100

    Update default users

--- src/main/java/org/polytech/covidapi/services/UserServices.java
+++ src/main/java/org/polytech/covidapi/services/UserServices.java
@@ -7,8 +7,11 @@ import java.util.Optional;
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 
+import org.polytech.covidapi.dao.CenterRepository;
 import org.polytech.covidapi.dao.UserRepository;
+import org.polytech.covidapi.entities.Center;
 import org.polytech.covidapi.entities.User;
+import org.polytech.covidapi.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,42 +24,43 @@ import org.springframework.stereotype.Service;
 public class UserServices implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final CenterRepository centerRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServices(final UserRepository utilisateurRepository, PasswordEncoder passwordEncoder) {
+    public UserServices(final UserRepository utilisateurRepository, CenterRepository centerRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = utilisateurRepository;
+        this.centerRepository = centerRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @PostConstruct
-    public void createUserDefault() {
-        User user = new User();
-        List<String> roles = new ArrayList<String>();
-        roles.add("USER");
-        user.setEmail("user@gmail.com");
-        user.setLastName("user");
-        user.setFirstName("user");
-        Optional<User> userSearch = userRepository.findFirstByEmail("user@gmail.com");
-        user.setRoles(roles);
-        user.setPassword(passwordEncoder.encode("password"));
-        if (!userSearch.isPresent()) {
-            this.userRepository.save(user);
+    public void createUserDefault() throws ResourceNotFoundException {
+        User patient = new User();
+        List<String> rolesPatient = new ArrayList<String>();
+        rolesPatient.add("PATIENT");
+        patient.setEmail("patient@gmail.com");
+        patient.setLastName("patient");
+        patient.setFirstName("patient");
+        Optional<User> patientSearch = userRepository.findFirstByEmail("patient@gmail.com");
+        patient.setRoles(rolesPatient);
+        patient.setPassword(passwordEncoder.encode("password"));
+        if (patientSearch.isEmpty()) {
+            this.userRepository.save(patient);
         }
 
-        User admin = new User();
-        admin.setEmail("admin@gmail.com");
-        admin.setLastName("admin");
-        admin.setFirstName("admin");
-        List<String> rolesAdmin = new ArrayList<String>();
-        rolesAdmin.add("ADMIN");
-        admin.setRoles(rolesAdmin);
-        admin.setPassword(passwordEncoder.encode("password"));
-        Optional<User> adminSearch = userRepository.findFirstByEmail("admin@gmail.com");
-        if (!adminSearch.isPresent()) {
-            this.userRepository.save(admin);
+        User superadmin = new User();
+        superadmin.setEmail("superadmin@gmail.com");
+        superadmin.setLastName("superadmin");
+        superadmin.setFirstName("superadmin");
+        List<String> rolesSuperadmin = new ArrayList<String>();
+        rolesSuperadmin.add("SUPERADMIN");
+        superadmin.setRoles(rolesSuperadmin);
+        superadmin.setPassword(passwordEncoder.encode("password"));
+        Optional<User> superadminSearch = userRepository.findFirstByEmail("superadmin@gmail.com");
+        if (superadminSearch.isEmpty()) {
+            this.userRepository.save(superadmin);
         }
-
     }
 
     @Override
